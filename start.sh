c10b2b5713949f42747f13de0b6c21f8249cea83
Luc Lagarde <mlaga97@gmail.com>
2022-10-11 18:19:08 -0500

    Comment start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-xkbcomp ./mirrorboard3.xkb $DISPLAY; xmodmap -e "keycode 65 = ISO_Level3_Shift"; xmodmap -e "keycode any = space"; xcape-1.2/xcape -e 'ISO_Level3_Shift=space'
+# Load wacky alternate 3rd level layout with mirroring
+xkbcomp ./mirrorboard3.xkb $DISPLAY
+
+# Virtual keycode for Level 3 Shift
+xmodmap -e "keycode 65 = ISO_Level3_Shift"
+
+# Disable spacebar
+xmodmap -e "keycode any = space"
+
+# Map long press of spacebar to level 3 shift
+xcape-1.2/xcape -e 'ISO_Level3_Shift=space'
